f9ee3a3456070ad324449ebaadebf57bc99f9182
Sudarvizhi-2003 <sudararam@gmail.com>
2025-08-10 19:41:45 +0530

    new changes

---
+++ practice/src/leetcode/longestPalindromeSubstring.java
@@ -0,0 +1,57 @@
+package leetcode;
+/*
+Given a string s, return the longest palindromic substring in s.
+
+
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ */
+public class longestPalindromeSubstring {
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        int maxLen = 1;
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + maxLen; j <= s.length(); j++) {
+                if (j - i > maxLen && isPalindrome(s.substring(i, j))) {
+                    maxLen = j - i;
+                    maxStr = s.substring(i, j);
+                }
+            }
+        }
+
+        return maxStr;
+    }
+
+    private boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        longestPalindromeSubstring lp = new longestPalindromeSubstring();
+        System.out.println(lp.longestPalindrome("babad"));
+    }
+}
