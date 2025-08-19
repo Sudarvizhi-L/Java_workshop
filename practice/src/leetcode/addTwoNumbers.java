928f9338ef5aa790cfe51e6205f1a45d97fcaf57
Sudarvizhi-2003 <sudararam@gmail.com>
2025-08-19 20:03:52 +0530

    file added

---
+++ practice/src/leetcode/addTwoNumbers.java
@@ -0,0 +1,63 @@
+package leetcode;
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ */
+
+public class addTwoNumbers {
+
+    public ListNode addNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum / 10;
+
+            tail.next = new ListNode(digit);
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        addTwoNumbers at = new addTwoNumbers();
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+
+        // Create second list: 5 -> 6 -> 4
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        // Add the two numbers
+        ListNode result = at.addNumbers(l1, l2);
+
+        // Print result
+        printList(result);
+    }
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+// ListNode class definition
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
