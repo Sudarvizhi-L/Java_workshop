5beacb4e2941384fbee7720d4f45b11a126d9ebe
Sudarvizhi-2003 <sudararam@gmail.com>
2025-08-26 21:49:41 +0530

    files added

---
+++ practice/src/leetcode/rotatedArray.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+public class rotatedArray {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        rotatedArray ra = new rotatedArray();
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(ra.search(nums, target));
+    }
+}
