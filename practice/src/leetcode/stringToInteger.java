d7f40290757874b065ad6a37aea2ac0a87b3b596
Sudarvizhi-2003 <sudararam@gmail.com>
2025-09-11 18:43:44 +0530

    files added

---
+++ practice/src/leetcode/stringToInteger.java
@@ -0,0 +1,81 @@
+package leetcode;
+/*
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+
+The algorithm for myAtoi(string s) is as follows:
+
+Whitespace: Ignore any leading whitespace (" ").
+Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
+Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+Return the integer as the final result.
+
+
+
+Example 1:
+
+Input: s = "42"
+
+Output: 42
+
+Explanation:
+
+The underlined characters are what is read in and the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+ */
+
+public class stringToInteger {
+    public int myAtoi(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        final int INT_MAX = Integer.MAX_VALUE;
+        final int INT_MIN = Integer.MIN_VALUE;
+
+        int i = 0;
+        int n = s.length();
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i == n) {
+            return 0;
+        }
+
+        int sign = 1;
+        if (s.charAt(i) == '+') {
+            i++;
+        } else if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+
+        long res = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            res = res * 10 + digit;
+
+            if (sign * res <= INT_MIN) {
+                return INT_MIN;
+            }
+            if (sign * res >= INT_MAX) {
+                return INT_MAX;
+            }
+
+            i++;
+        }
+
+        return (int)(res * sign);
+    }
+
+    public static void main(String[] args) {
+        stringToInteger sti = new stringToInteger();
+        System.out.println(sti.myAtoi("44"));
+    }
+}
